68c6d3fde92173d368bcb7bc18e85d29c4d07796
rhahao.vj <26148770+rhahao@users.noreply.github.com>
2025-01-29 07:46:44 +0000

    feat(users): add user icon for admin

--- src/features/users/label/index.tsx
+++ src/features/users/label/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography } from '@mui/material';
-import { AccountCircle, AdminPanelSettings } from '@mui/icons-material';
+import { AccountCircle, AdminPanelSettings, StarsRounded } from '@mui/icons-material';
 import { LabelProps } from './index.types';
 
 const Label = ({ role, fullname }: LabelProps) => {
@@ -7,6 +7,7 @@ const Label = ({ role, fullname }: LabelProps) => {
     <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
       {role === 'vip' && <AdminPanelSettings color="primary" />}
       {role === 'pocket' && <AccountCircle color="secondary" />}
+      {role === 'admin' && <StarsRounded color="error" />}
 
       <Typography>{fullname}</Typography>
     </Box>
